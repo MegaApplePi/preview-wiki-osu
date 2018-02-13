f22752b09cfaabed9160fe5bdd56509a0a7ff756
Eric X <123MegaApple123@gmail.com>
2018-02-13 16:01:36 -0600

    group (sub)title functions and fix GitHub links
    fixes the ability to load images from GitHub

--- assets/js/inspectDOM.js
+++ assets/js/inspectDOM.js
@@ -32,33 +32,37 @@ export default function inspectDOM() {
 
   /* get the title and subtitle */
   // NOTE we believe that osu!wiki uses the first h1 element, then uses the first heading element, then uses the URL
-  let tagH1; // placeholder for the h1 element (undefined == false)
-  for (let element of headings) {
-    if (element.tagName === "H1") {
-      tagH1 = element; // if it exists, assign a reference to it (element == true)
-      break;
+  (function() {
+    let tagH1; // placeholder for the h1 element (undefined == false)
+    for (let element of headings) {
+      if (element.tagName === "H1") {
+        tagH1 = element; // if it exists, assign a reference to it (element == true)
+        break;
+      }
     }
-  }
-  if (tagH1) { // use <h1/>, if it exists
-    $wikiHeaderTitle.textContent = tagH1.textContent;
-    tagH1.remove();
-  } else if (headings[0]) { // use the first h# tag, if it exist
-    $wikiHeaderTitle.textContent = headings[0].textContent;
-    headings[0].remove();
-  } else { // just use the URL
-    let pathParts = $toolbarPath.value.split(/\\|\//);
-    let title = pathParts[pathParts.length - 2].replace(/_/g, " ");
-    $wikiHeaderTitle.textContent = title;
-  }
+    if (tagH1) { // use <h1/>, if it exists
+      $wikiHeaderTitle.textContent = tagH1.textContent;
+      tagH1.remove();
+    } else if (headings[0]) { // use the first h# tag, if it exist
+      $wikiHeaderTitle.textContent = headings[0].textContent;
+      headings[0].remove();
+    } else { // just use the URL
+      let pathParts = $toolbarPath.value.split(/\\|\//);
+      let title = pathParts[pathParts.length - 2].replace(/_/g, " ");
+      $wikiHeaderTitle.textContent = title;
+    }
+  })();
 
   // NOTE we believe that the subtitle is obtained from the URL
-  let pathParts = $toolbarPath.value.split(/\\|\//);
-  let subtitle = pathParts[pathParts.length - 3].replace(/_/g, " ");
-  if (subtitle === "wiki") {
-    $wikiHeaderSubtitle.textContent = "";
-  } else {
-    $wikiHeaderSubtitle.textContent = subtitle;
-  }
+  (function() {
+    let pathParts = $toolbarPath.value.split(/\\|\//);
+    let subtitle = pathParts[pathParts.length - 3].replace(/_/g, " ");
+    if (subtitle === "wiki") {
+      $wikiHeaderSubtitle.textContent = "";
+    } else {
+      $wikiHeaderSubtitle.textContent = subtitle;
+    }
+  })();
 
   /* TOC */
   headings = getHeadings(); // get a new sample of the headings
@@ -92,13 +96,20 @@ export default function inspectDOM() {
   }
   headings = null; // nullify headings (we are done with it)
 
-  // TODO check path to see if github, if so, use github to get images
   /* images */
+
   let images = getImages();
   for (let element of images) {
     let src = element.getAttribute("src");
     src = src.replace(/\u03A0/g, "_"); // convert \u03A0 (π) back to underscores
-    if (/^(https?|mailto):/.test(src)) {
+    if (/^https?:\/\/github.com/.test($toolbarPath.value)) {
+      let pathParts = $toolbarPath.value.split(/\\|\//);
+      pathParts[pathParts.indexOf("github.com")] = "raw.githubusercontent.com"; // change domain to GitHub's raw domain
+      pathParts.splice(pathParts.indexOf("blob"), 1);// remove /blob/ in URL
+
+      let newURL = pathParts.splice(0, pathParts.indexOf("wiki")).join("/");// the new URL
+      src = `${newURL}${src}`;
+    } else if (/^(https?|mailto):/.test(src)) {
       continue;
     } else if (/^\//.test(src)) {
       let filePath = $toolbarPath.getAttribute("data-value").split(/\\|\//g);
@@ -110,6 +121,7 @@ export default function inspectDOM() {
       filePath = filePath.slice(0, filePath.length - 1).join("/");
       src = path.resolve(filePath, src);
     }
+
     element.src = src;
     let hasTitle = element.hasAttribute("title") && (element.getAttribute("title").length > 0);
     let isAlone = (element.parentElement.children.length === 1) && (element.parentElement.textContent.length === 0);
