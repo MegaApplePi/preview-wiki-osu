492e23891883701936a5907d3019c21dda0ae960
Eric X <123MegaApple123@gmail.com>
2018-05-02 01:24:42 -0500

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,39 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
 module.exports = {
-	"entry": "./assets/js/app.js",
-	"externals": [
-		"electron",
-		"fs"
-	],
-	"output": {
-		"filename": "index.js"
-	},
-	"watch": true,
-	"watchOptions":{
-		"ignored": /node_modules/
-	}
+  "entry": "./assets/js/app.js",
+  "externals": ["electron", "fs"],
+  "output": {
+    "filename": "index.js"
+  },
+  "watch": true,
+  "watchOptions": {
+    "ignored": /node_modules/
+  },
+  "plugins": [
+    new UglifyJsPlugin()
+  ]
+  // ],
+  // "module": {
+  //   "rules": [
+  //     {
+  //       "test": /\.scss$/,
+  //       "use": [
+  //         {
+  //           "loader": "style-loader"
+  //         },
+  //         {
+  //           "loader": "css-loader"
+  //         },
+  //         {
+  //           "loader": "sass-loader",
+  //           "options":{
+  //             "file": "assets/scss/index.scss",
+  //             "outFile": "assets/css/index.css"
+  //           }
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
 };
