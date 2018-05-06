31735b21e3450e1e8a4c1ccf5ade6cc951778a06
Eric X <123MegaApple123@gmail.com>
2018-05-05 21:15:34 -0500

    fix TOC math
    This should ensure that the TOC stops moving if it is too long

--- resources/js/$wiki_scroll.js
+++ resources/js/$wiki_scroll.js
@@ -5,13 +5,13 @@ export default function $wiki_scroll() {
   // NOTE this is probably not the most optimal way to do this, but it works... in previous attempts, using postition fixed breaks it
 
   // check if the TOC is shorter than the window height
-  if ($wikiBodyPageTocContent.getBoundingClientRect().height < window.innerHeight) {
+  if ($wikiBodyPageTocContent.getBoundingClientRect().height < window.innerHeight - 36) {
     // if so, the TOC scrolls with the page
 
     // check if the page is scrolled to a different scroll position
-    if ($wikiBodyPageToc.getBoundingClientRect().top < $wiki.scrollTop && $wiki.scrollTop - 156 > 0) {
+    if ($wikiBodyPageToc.getBoundingClientRect().top < $wiki.scrollTop && $wiki.scrollTop - 164 > 0) {
       // if so, set the TOC's top to that position
-      $wikiBodyPageTocContent.style.top = `${$wiki.scrollTop - 156}px`;
+      $wikiBodyPageTocContent.style.top = `${$wiki.scrollTop - 164}px`;
     } else if ($wiki.scrollTop === 0) {
       // if wiki is scrolled to the very top, put the TOC back (otherwise it may go up too much)
       $wikiBodyPageTocContent.style.top = "0";
